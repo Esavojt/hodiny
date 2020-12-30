a61906576290692c417751ea8670682c99634487
Vojta Otáhal <vojta.otahal@seznam.cz>
2020-12-30 16:08:40 +0100

    added autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,16 +4,16 @@ if ! [ $(id -u) = 0 ]; then
     exit 1
 fi
 
-echo "Updating apt repository"
+echo "---------- Updating apt repository ----------"
 apt update
 
-echo "Installing python3"
-apt install python3 python3-pip python3-venv python3-distutils
+echo "---------- Installing python3 ----------"
+apt install python3 python3-pip python3-venv python3-distutils -y
 
-echo "Installing needed libraries"
-apt install git
+echo "---------- Installing needed libraries ----------"
+apt install git -y
 
-echo "Installing GPIO related libraries"
+echo "---------- Installing GPIO related libraries ----------"
 pip3 install rpi_ws281x
 pip3 install RPi.GPIO
 
@@ -23,7 +23,7 @@ python3 setup.py install
 
 cd ..
 
-echo "Cloning repository"
+echo "---------- Cloning repository ----------"
 git clone https://github.com/Esavojt/hodiny.git
 
 cd hodiny
