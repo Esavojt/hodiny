eb68020d3ac67e6b408fa3f5afa841ba02bd82a6
Vojta <vojta.otahal@seznam.cz>
2020-12-30 16:01:18 +0100

    fixed autoinstall

---
+++ autoinstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+    echo "You need to run this script as root!"
+    exit 1
+fi
+
+echo "Updating apt repository"
+apt update
+
+echo "Installing python3"
+apt install python3 python3-pip python3-venv python3-distutils
+
+echo "Installing needed libraries"
+apt install git
+
+echo "Installing GPIO related libraries"
+pip3 install rpi_ws281x
+pip3 install RPi.GPIO
+
+git clone https://github.com/simonmonk/pi_analog.git
+cd pi_analog
+python3 setup.py install
+
+cd ..
+
+echo "Cloning repository"
+git clone https://github.com/Esavojt/hodiny.git
+
+cd hodiny
