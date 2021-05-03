e74146c69fc922d6e613fb3e152097b68d5e62d7
Vojta Otáhal <vojta.otahal@seznam.cz>
2021-05-03 10:57:41 +0200

    e

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ websocket_server = WebSocketServer(queueWC2WS, queueWS2WC, asyncio.get_event_loo
 
 if __name__ == '__main__':
     """Get update"""
-    os.system("git checkout -- .")
     os.system("git pull origin main")
     
     web_server.start()
